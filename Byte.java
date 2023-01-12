450fba28ef9f004c71baa5633b7415dddc96d778
monty123naveen <nvkumar1229@gmail.com>
2023-01-12 08:53:35 +0530

    Datatypes

---
+++ Byte.java
@@ -0,0 +1,12 @@
+public class Byte {
+
+  public static void main(String[] args) {
+    
+    Byte a = 1;
+    Byte b = 2;
+
+    
+    Byte sum = a + b;
+    System.out.println(a + " + " + b + " = "  + sum);
+  }
+}
---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+public class Datatypes
+{
+    public static void dataTypes(int a, float b, double c, long l, byte d){
+        
+         p =  Divide ;c/b.
+         q = Divide ;c/a
+         r = Divide ;c/a
+         m = ;r+l 
+         s = ;Divide a/d
+        
+         
+        System.out.println(p + " " + q + " " + r + " " + m + " " + s);
+        
+    }
+}
+
+
